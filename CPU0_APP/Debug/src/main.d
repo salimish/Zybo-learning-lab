caa335b6f8ac742c4f2c081798b0ee7058b4fe41
salimish <atefeh.salimi@gmail.com>
2025-12-06 13:17:31 +0330

    Add files via upload

---
+++ CPU0_APP/Debug/src/main.d
@@ -0,0 +1,75 @@
+src/main.o src/main.o: ../src/main.c \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xparameters.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xscugic.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xstatus.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xil_types.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xil_assert.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xil_io.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xil_printf.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xparameters.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/bspconfig.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xil_exception.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xgpiops.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xgpiops_hw.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xplatform_info.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xil_printf.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xil_exception.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xil_mmu.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xpseudo_asm.h ../src/share.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xil_cache.h \
+ ../../CPU0_BSP/ps7_cortexa9_0/include/xtime_l.h
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xparameters.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xscugic.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xstatus.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xil_types.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xil_assert.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xil_io.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xil_printf.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xparameters.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/bspconfig.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xil_exception.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xgpiops.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xgpiops_hw.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xplatform_info.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xil_printf.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xil_exception.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xil_mmu.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../src/share.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xil_cache.h:
+
+../../CPU0_BSP/ps7_cortexa9_0/include/xtime_l.h:
